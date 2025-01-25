7352efc51c744810a54f212ebc0d35e0708ef6ec
raresndc <rares.ndc@gmail.com>
2025-01-25 13:08:29 +0200

    finished feb2022Subj

--- Java/Exam_prep/Feb2022Subj/src/Main.java
+++ Java/Exam_prep/Feb2022Subj/src/Main.java
@@ -5,11 +5,14 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 public class Main {
 
@@ -77,7 +80,117 @@ public class Main {
         fos.close();
     }
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+    public static KeyStore getKeyStore(
+            String keyStoreFile,
+            String keyStorePass,
+            String keyStoreType) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        File file = new File(keyStoreFile);
+        if(!file.exists()) {
+            throw new UnsupportedOperationException("Missing key store file");
+        }
+
+        FileInputStream fis = new FileInputStream(file);
+
+        KeyStore ks = KeyStore.getInstance(keyStoreType);
+        ks.load(fis, keyStorePass.toCharArray());
+
+        fis.close();
+        return ks;
+    }
+
+    public static void list(KeyStore ks) throws KeyStoreException {
+        System.out.println("Key store content: ");
+        Enumeration<String> aliases = ks.aliases();
+
+        while(aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            System.out.println("Entry: " + alias);
+            if(ks.isCertificateEntry(alias)) {
+                System.out.println("-- Is a certificate");
+            }
+            if(ks.isKeyEntry(alias)) {
+                System.out.println("-- Is a key pair");
+            }
+        }
+    }
+
+    public static PublicKey getPublicKey(String alias, KeyStore ks) throws KeyStoreException {
+        if(ks == null) {
+            throw new UnsupportedOperationException("Missing Key Store");
+        }
+        if(ks.containsAlias(alias)) {
+            return ks.getCertificate(alias).getPublicKey();
+        } else {
+            return null;
+        }
+    }
+
+    public static PrivateKey getPrivateKey(
+            String alias, String keyPass, KeyStore ks ) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+        if(ks == null) {
+            throw new UnsupportedOperationException("Missing Key Store");
+        }
+        if(ks.containsAlias(alias)) {
+            return (PrivateKey) ks.getKey(alias, keyPass.toCharArray());
+        } else {
+            return null;
+        }
+    }
+
+    public static PublicKey getCertificateKey(String certificateFile) throws CertificateException, IOException {
+        File file = new File(certificateFile);
+        if(!file.exists()) {
+            throw new UnsupportedOperationException("****Missing file****");
+        }
+        FileInputStream fis = new FileInputStream(file);
+
+        CertificateFactory certFactory =
+                CertificateFactory.getInstance("X.509");
+        X509Certificate certificate =
+                (X509Certificate) certFactory.generateCertificate(fis);
+        fis.close();
+        return certificate.getPublicKey();
+    }
+
+    public static byte[] signFile(String filename, PrivateKey key) throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        File file = new File(filename);
+        if(!file.exists()) {
+            throw new FileNotFoundException();
+        }
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] fileContent = fis.readAllBytes();
+
+        fis.close();
+
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(key);
+
+        signature.update(fileContent);
+        return signature.sign();
+    }
+
+    public static boolean hasValidSignature(
+            String filename, PublicKey key, byte[] signature) throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+
+        File file = new File(filename);
+        if(!file.exists()) {
+            throw new FileNotFoundException();
+        }
+
+        FileInputStream fis = new FileInputStream(file);
+        byte[] fileContent = fis.readAllBytes();
+        fis.close();
+
+        Signature signatureModule = Signature.getInstance("SHA256withRSA");
+        signatureModule.initVerify(key);
+
+        signatureModule.update(fileContent);
+        return signatureModule.verify(signature);
+
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, KeyStoreException, UnrecoverableKeyException, CertificateException, SignatureException {
         File file = new File("Passphrase.txt");
         byte[] password = new byte[0];
         if(!file.exists()) {
@@ -97,5 +210,38 @@ public class Main {
         decrypt("EncryptedData.data", "OriginalData.txt", pass, "AES");
 
         //to do the last one
+
+        KeyStore ks = getKeyStore(
+                "sap_exam_keystore.ks", "you_already_made_it", "pkcs12");
+        list(ks);
+
+        PublicKey pubIsm1 = getPublicKey("sapexamkey", ks);
+        PrivateKey privIsm1 = getPrivateKey("sapexamkey", "you_already_made_it", ks);
+
+        System.out.println("Public key:");
+        System.out.println(getHexString(pubIsm1.getEncoded()));
+        System.out.println("Private key");
+        System.out.println(getHexString(privIsm1.getEncoded()));
+
+        PublicKey pubIsm1FromCert =
+                getCertificateKey("ISMCertificateX509.cer");
+        System.out.println("Public key from certificate: ");
+        System.out.println(getHexString(pubIsm1FromCert.getEncoded()));
+
+        byte[] signature =
+                signFile("OriginalData.txt", privIsm1);
+        File signatureFile = new File("DataSignature.ds");
+        Files.write(Path.of(signatureFile.getAbsolutePath()), signature);
+
+        System.out.println("Digital signature value: ");
+        System.out.println(getHexString(signature));
+
+        if(hasValidSignature(
+                "OriginalData.txt", pubIsm1FromCert, signature))
+        {
+            System.out.println("File is the original one");
+        } else {
+            System.out.println("File has been changed");
+        }
     }
 }
